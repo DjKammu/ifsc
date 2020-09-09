cb7ab32e52b1a0b25e513c24de9840236549f12b
Kamlesh DjKammu <dhundharakamlesh@gmail.com>
2020-09-09 16:58:46 +0530

    Wip

--- app/Admin/Controllers/LocationController.php
+++ app/Admin/Controllers/LocationController.php
@@ -27,10 +27,18 @@ class LocationController extends AdminController
         $grid = new Grid(new Location());
 
         $grid->column('id', __('Id'));
-        $grid->column('path', __('Path'));
+        $grid->column('path', __('Path'))->link();
         $grid->column('count', __('Count'));
         $grid->column('ip', __('Ip'));
-        $grid->column('location_data', __('Location data'));
+        $grid->column('location_data', __('Location data'))->display( function($data) {
+              if(empty($data)){ return;}
+              $data = json_decode($data,true);
+              $htm = 'Country :<b> '.$data['countryName'].'</b></br>';
+              $htm .= 'Region :<b> '.$data['regionName'].'</b></br>';
+              $htm .= 'City :<b> '.$data['cityName'].'</b></br>';
+              $htm .= 'Zip Code :<b> '.$data['zipCode'].'</b>';
+              return $htm;
+        });
         // $grid->column('created_at', __('Created at'));
         // $grid->column('updated_at', __('Updated at'));
 
@@ -48,10 +56,18 @@ class LocationController extends AdminController
         $show = new Show(Location::findOrFail($id));
 
         $show->field('id', __('Id'));
-        $show->field('path', __('Path'));
+        $show->field('path', __('Path'))->link();
         $show->field('count', __('Count'));
         $show->field('ip', __('Ip'));
-        $show->field('location_data', __('Location data'));
+        $show->field('location_data', __('Location data'))->as( function($data) {
+              if(empty($data)){ return;}
+              $data = json_decode($data,true);
+              $htm = 'Country : '.$data['countryName'].', ';
+              $htm .= 'Region : '.$data['regionName'].', ';
+              $htm .= 'City : '.$data['cityName'].', ';
+              $htm .= 'Zip Code : '.$data['zipCode'];
+              return $htm;
+        });
         $show->field('created_at', __('Created at'));
         $show->field('updated_at', __('Updated at'));
 
